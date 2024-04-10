b42e091c14590f506fa753a56c33726bd1ea46f9
Bpost129 <Bpost129@yahoo.com>
2024-04-10 17:50:46 -0400

    update skill-data

--- data/skill-data.js
+++ data/skill-data.js
@@ -1,7 +1,8 @@
 const skills = [
-  {text: 'Feed llama', done: true, _id: 125223},
-  {text: 'Sleep under the stars', done: false, _id: 127904},
-  {text: 'Buy milk', done: false, _id: 139608},
+  {text: 'for loops', done: true, _id: 125223},
+  {text: 'event listeners', done: true, _id: 127904},
+  {text: 'recursion', done: false, _id: 139608},
+  {text: 'git', done: true, _id: 137699},
 ]
 
 export { 
